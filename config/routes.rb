f3c5daa6e1efb6daa3a58a39ef971227608d3246
BENFATIH Youness <youngesida@gmail.com>
2018-11-30 11:52:15 +0100

    adding the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :producttypes
+
+  get "/dashboard" => "pages#home"
 end
