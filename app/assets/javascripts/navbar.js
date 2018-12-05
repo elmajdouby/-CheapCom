1d693a77552975ecd97a042bbedf06e11b2e890e
BENFATIH Youness <youngesida@gmail.com>
2018-12-05 16:47:08 +0100

    navbar & products categories

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,8 +1,7 @@
  window.onscroll = function() {scrollNav()};
-
+ document.getElementById("show_categories_navbar").onclick = function(){show_categories_navbar()};
 
 function scrollNav(){
-
 const navbar = document.querySelector('.navbar-wagon');
 const search_nav = document.getElementById('search_nav');
 const location_nav = document.getElementById('btn-location');
@@ -28,3 +27,20 @@ console.log(search_nav);
     });
   }
 }
+
+function show_categories_navbar(){
+
+  const categories_nav = document.getElementById('categories-navbar');
+  categories_nav.classList.remove('categories-navbar-hide');
+  console.log(categories_nav);
+   // if (categories_nav ) {
+   //  window.addEventListener('scroll', () => {
+   //    if (window.scrollY > window.innerHeight /2) {
+   //      categories_nav.classList.remove('categories-navbar-show');
+   //    } else {
+   //      categories_nav.classList.add('categories-navbar-show');
+   //    }
+   //  });
+  // }
+}
+
