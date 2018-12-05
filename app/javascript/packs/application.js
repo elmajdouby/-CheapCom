49daeb3e0de02e72c4fae3b0df0064df0d0f3261
jafouri <jaf.salah@gmail.com>
2018-12-05 16:47:21 +0100

    salaheddine

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+import Drift from 'drift-zoom';
+
+new Drift(document.querySelector('.image-zoom'), {
+  inlinePane: 900,
+  inlineOffsetY: 85,
+  zoomFactor: 2,
+  containInline: true,
+  hoverBoundingBox: true
+});
