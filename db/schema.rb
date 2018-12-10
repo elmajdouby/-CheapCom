122f0f7d964b0e349cddf9ecdff680df1b05f4f2
Khalid EL MAJDOUBY <k.elmajdouby@gmail.com>
2018-12-10 08:52:25 +0000

    dd

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_12_07_105206) do
-
-
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
