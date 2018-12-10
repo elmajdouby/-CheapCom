fc35874a4eafb73333de0cdd49e97f653b7a60d0
Khalid EL MAJDOUBY <k.elmajdouby@gmail.com>
2018-12-10 08:59:12 +0000

    d

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_12_07_105206) do
-
+ActiveRecord::Schema.define(version: 2018_12_05_172421) do
 
 
 # ActiveRecord::Schema.define(version: 2018_12_07_105206) do
